bc686d4ad2887ed1b3988ff60256aaff795e92d4
Miles Possing <milespossing@microsoft.com>
2022-12-04 19:26:45 -0800

    Day4 pt2

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 struct Elf {
     lower: u8,
-    upper: u8
+    upper: u8,
 }
 
 impl FromStr for Elf {
@@ -16,22 +16,26 @@ impl FromStr for Elf {
     }
 }
 
-pub fn solve(p: &std::path::Path) -> Result<String, &str> {
+pub fn solve(p: &std::path::Path) -> Result<(i32, i32), &str> {
     let file = std::fs::read_to_string(p.join("day04.txt")).unwrap();
-    let result = file
-        .split('\n')
-        .filter(|&s| !s.is_empty())
-        .map(|s| { 
-            let splt: Vec<&str> = s.split(',').collect();
-            return (Elf::from_str(splt[0]).unwrap(), Elf::from_str(splt[1]).unwrap())
-        })
-        .filter(|input| { match input {
-            (left, right)
-                if left.lower <= right.lower && left.upper >= right.upper => true,
-            (left, right)
-                if right.lower <= left.lower && right.upper >= left.upper => true,
-            _ => false,
-        }})
-        .count();
-    Ok(format!("{}", result))
+    let (r1, r2) = file.split('\n').filter(|&s| !s.is_empty()).map(|s| {
+        let splt: Vec<&str> = s.split(',').collect();
+        return (
+            Elf::from_str(splt[0]).unwrap(),
+            Elf::from_str(splt[1]).unwrap(),
+        );
+    })
+    .fold((0,0), |(r1, r2), input| {
+        let is_contained: i32 = match &input {
+            (left, right) if left.lower <= right.lower && left.upper >= right.upper => 1,
+            (left, right) if right.lower <= left.lower && right.upper >= left.upper => 1,
+            _ => 0,
+        };
+        let overlap: i32 = match input {
+            (left, right) if left.lower > right.upper || left.upper < right.lower => 0,
+            _ => 1
+        };
+        (r1 + is_contained, r2 + overlap)
+    });
+    Ok((r1, r2))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     println!("Day 01: {}", days::day01::solve_01().unwrap());
     println!("Day 02: {}", days::day02::solve(std::path::Path::new("./inputs")).unwrap());
     println!("Day 03: {}", days::day03::solve(std::path::Path::new("./inputs")).unwrap());
-    println!("Day 04: {}", days::day04::solve(std::path::Path::new("./inputs")).unwrap());
+    let (d4_1, d4_2) = days::day04::solve(std::path::Path::new("./inputs")).unwrap();
+    println!("Day 04: pt1: {} pt2: {}", d4_1, d4_2);
 }
